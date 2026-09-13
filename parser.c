a5387494fdc6427a54853e83bc8c97ad29bf0cd5
ksymmons <kazukisymmons@gmail.com>
2026-09-14 07:18:08 +0900

    added struct for paths

--- parser.c
+++ parser.c
@@ -1,8 +1,10 @@
 #include <linux/limits.h>
 #include <stddef.h>
-# include <stdio.h>
-# include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <limits.h>
@@ -15,7 +17,16 @@ typedef struct {
 	time_t now;
 }t_save;
 
-int get_path(char *data_path)
+// I know I should not use PATH_MAX like an ape, but im too lazy to fix this
+typedef struct
+{
+	char data_path[PATH_MAX];
+	char log_path[PATH_MAX];
+	char save_path[PATH_MAX];
+}t_paths;
+
+// function to get where to put save files
+int get_path(t_paths *paths)
 {
 	const char *xdg_home;
 	const char *home;
@@ -23,32 +34,45 @@ int get_path(char *data_path)
 	xdg_home = getenv("XDG_DATA_HOME");
 
 	if (xdg_home != NULL && *xdg_home != '\0')
-		snprintf(data_path, PATH_MAX, "%s/gitplant", xdg_home);
+		snprintf(paths->data_path, PATH_MAX, "%s/gitplant", xdg_home);
 	else
 	{
 		home = getenv("HOME");
 		if (home == NULL)
 			return (-1);
-		snprintf(data_path, PATH_MAX, "%s/.local/share/gitplant", home);
+		snprintf(paths->data_path, PATH_MAX, "%s/.local/share/gitplant", home);
 	}
 	return (0);
 }
 
+void get_log_path(t_paths *paths)
+{
+	strcpy(paths->log_path, paths->data_path);
+	strcat(paths->log_path, "/events.txt");
+}
 
-int count_commit(char *data_path)
+void get_save_path(t_paths *paths)
+{
+	strcpy(paths->save_path, paths->data_path);
+	strcat(paths->save_path, "/save.txt");
+}
+
+
+
+int count_commit(t_paths *paths)
 {
 	int fd;
 	char buf[1024];
 	ssize_t n;
-	char logpath[4096];
 	int count = 0;
 
-	strcpy(logpath, data_path);
-	strcat(logpath, "/events.txt");
 
-	fd = open(logpath, O_RDONLY);
+	fd = open(paths->log_path, O_RDONLY);
 	if(fd == -1)
-		return ( 0);
+	{
+		printf("failed to read file\n");
+		return (0);
+	}
 	while((n = read(fd,buf,sizeof(buf))) > 0)
 	{
 		int i = 0;
@@ -64,13 +88,29 @@ int count_commit(char *data_path)
 }
 
 
+/* time_t get_commit_time(char , int commit_line) */
+/* { */
+/* 	while(commit_line) */
+/* 	{ */
+
+/* 	} */
+/* } */
+
 
 int main()
 {
-	char data_path[PATH_MAX];
-	get_path(data_path);
 	t_save save;
-	save.commits = count_commit(data_path);
+	t_paths paths;
+	get_path(&paths);
+	get_log_path(&paths);
+	get_save_path(&paths);
+	save.commits = count_commit(&paths);
+	save.now = time(NULL);
 
-	printf("%zu", save.commits);
+	printf("%s\n", paths.data_path);
+	printf("%s\n", paths.save_path);
+	printf("%s\n", paths.log_path);
+
+	printf("%zu\n", save.commits);
+	printf("%jd\n", (intmax_t)save.now);
 }
