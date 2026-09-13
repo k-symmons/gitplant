225d7755b20d8131f01c29c6fa8d76c5a325e8f5
ksymmons <kazukisymmons@gmail.com>
2026-09-14 06:10:43 +0900

    read save file and use as variable

--- init.sh
+++ init.sh
@@ -4,7 +4,6 @@ mkdir -p ~/.config/git/hooks && touch ~/.config/git/hooks/post-commit
 
 cp post-commit ~/.config/git/hooks/post-commit
 
-
 git config --global core.hooksPath ~/.config/git/hooks
 
 chmod u=wrx ~/.config/git/hooks/post-commit
--- parser.c
+++ parser.c
@@ -1,12 +1,21 @@
+#include <linux/limits.h>
+#include <stddef.h>
 # include <stdio.h>
 # include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <limits.h>
 
-char g_data_path[4096];
 
-int get_path(void)
+typedef struct {
+	size_t commits;
+	size_t points;
+	time_t last_commit;
+	time_t now;
+}t_save;
+
+int get_path(char *data_path)
 {
 	const char *xdg_home;
 	const char *home;
@@ -14,19 +23,19 @@ int get_path(void)
 	xdg_home = getenv("XDG_DATA_HOME");
 
 	if (xdg_home != NULL && *xdg_home != '\0')
-		snprintf(g_data_path, sizeof(g_data_path), "%s/gitplant", xdg_home);
+		snprintf(data_path, PATH_MAX, "%s/gitplant", xdg_home);
 	else
 	{
 		home = getenv("HOME");
 		if (home == NULL)
 			return (-1);
-		snprintf(g_data_path, sizeof(g_data_path), "%s/.local/share/gitplant", home);
+		snprintf(data_path, PATH_MAX, "%s/.local/share/gitplant", home);
 	}
 	return (0);
 }
 
 
-int count_commit()
+int count_commit(char *data_path)
 {
 	int fd;
 	char buf[1024];
@@ -34,28 +43,34 @@ int count_commit()
 	char logpath[4096];
 	int count = 0;
 
-	strcpy(logpath, g_data_path);
+	strcpy(logpath, data_path);
 	strcat(logpath, "/events.txt");
 
 	fd = open(logpath, O_RDONLY);
 	if(fd == -1)
-		return (close(fd), 0);
-	while((n = read(fd,buf,sizeof(buf)-1) > 0))
+		return ( 0);
+	while((n = read(fd,buf,sizeof(buf))) > 0)
 	{
 		int i = 0;
-		while(buf[i] != '\0')
+		while(i < n)
 		{
 			if (buf[i] == '\n')
 				count ++;
 			i ++;
 		}
 	}
+	close(fd);
 	return (count);
 }
 
 
+
 int main()
 {
-	get_path();
-	printf("%d", count_commit());
+	char data_path[PATH_MAX];
+	get_path(data_path);
+	t_save save;
+	save.commits = count_commit(data_path);
+
+	printf("%zu", save.commits);
 }
