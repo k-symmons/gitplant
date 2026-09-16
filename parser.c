0919738ffdbf7776182472ce4749192426d34a50
k-symmons <kazukisymmons+dev@gmail.com>
2026-09-17 04:22:37 +0900

    added game logic(test commit)

--- parser.c
+++ parser.c
@@ -1,5 +1,4 @@
 # include "plant.h"
-#include <string.h>
 
 
 
@@ -172,19 +171,31 @@ int main()
 	get_path(&paths);
 	get_log_path(&paths);
 	get_save_path(&paths);
-	save.now = time(NULL);
-	/* read_save(&paths, &save); */
+
+	int multi_base = 20;
+	int multi_24h = 10;
+	int c_24h_sec = 86400;
 
 
+	while (1)
+	{
+		save.now = time(NULL);
+		read_save(&paths, &save);
+		int log_commit_count = count_commit(&paths);
+		if((int)save.commits < log_commit_count)
+		{
+			int log_last_commit = get_commit_time(&paths, log_commit_count);
+			if (log_last_commit - save.last_commit > c_24h_sec)
+				save.points += (log_commit_count-save.commits) *multi_base * multi_24h;
+			else
+				save.points += (log_commit_count-save.commits) *multi_base;
+			// bug: if the newest commit is older than 24h compared to save.last_commit, all commits after last commits get multi_24h
+			save.last_commit = log_last_commit;
+			write(1, "change", 6);
+		}
+		write_save(&paths, &save);
+		write(1, "looping", 7);
+		sleep(300);
+	}
 
-
-
-	save.commits = count_commit(&paths);
-	save.last_commit = get_commit_time(&paths, save.commits);
-	save.points = 3;
-
-	printf("%d\n", (int)save.commits);
-	printf("%d\n", (int)save.last_commit);
-	printf("%d\n", (int)save.points);
-	write_save(&paths, &save);
 }
