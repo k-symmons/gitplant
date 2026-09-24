b954e50fd4194b4f816a011c24f94edd08cbe6d4
ksymmons <kazukisymmons@gmail.com>
2026-09-24 19:24:13 +0900

    move main to separate file

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main()
 	get_log_path(&paths);
 	get_save_path(&paths);
 
-	int multi_base = 20;
+	int multi_base = 10;
 	int multi_24h = 10;
 	int c_24h_sec = 86400;
 
@@ -24,15 +24,14 @@ int main()
 		{
 			int log_last_commit = get_commit_time(&paths, log_commit_count);
 			if (log_last_commit - save.last_commit > c_24h_sec)
-				save.points += (log_commit_count-save.commits) *multi_base * multi_24h;
+				save.points += (log_commit_count-save.commits-1) *multi_base + (multi_base * multi_24h); //after 24h newest commit get a aditional multi
 			else
 				save.points += (log_commit_count-save.commits) *multi_base;
-			// bug: if the newest commit is older than 24h compared to save.last_commit, all commits after last commits get multi_24h
 			save.commits = log_commit_count;
 			save.last_commit = log_last_commit;
 			draw_plant(save.points / 1000);
 		}
 		write_save(&paths, &save);
-		sleep(300);
+		sleep(5);
 	}
 }
--- parser.c
+++ parser.c
@@ -1,4 +1,5 @@
 # include "plant.h"
+#include <stdlib.h>
 
 
 
@@ -111,14 +112,18 @@ char *stoa(long long num)
 	int digit_count = 0;
 	char *string;
 
-
-
-	while(num/digit > 0)
+	if (num != 0)
 	{
-		digit_count++;
-		digit *= 10;
+		while(num/digit > 0)
+		{
+			digit_count++;
+			digit *= 10;
+		}
 	}
-	string = malloc(sizeof(char) * digit_count + 1);
+	else
+		digit_count = 0;
+
+	string = calloc(digit_count +2, sizeof(char));
 
 	if(string == 0)
 	{
@@ -165,39 +170,3 @@ void write_save(t_paths *paths, t_save *save)
 }
 
 
-int main()
-{
-	t_save save;
-	t_paths paths;
-	get_path(&paths);
-	get_log_path(&paths);
-	get_save_path(&paths);
-
-	int multi_base = 20;
-	int multi_24h = 10;
-	int c_24h_sec = 86400;
-
-
-	while (1)
-	{
-		save.now = time(NULL);
-		read_save(&paths, &save);
-		int log_commit_count = count_commit(&paths);
-		if((int)save.commits < log_commit_count)
-		{
-			int log_last_commit = get_commit_time(&paths, log_commit_count);
-			if (log_last_commit - save.last_commit > c_24h_sec)
-				save.points += (log_commit_count-save.commits) *multi_base * multi_24h;
-			else
-				save.points += (log_commit_count-save.commits) *multi_base;
-			// bug: if the newest commit is older than 24h compared to save.last_commit, all commits after last commits get multi_24h
-			save.commits = log_commit_count;
-			save.last_commit = log_last_commit;
-			write(1, "change\n", 7);
-		}
-		write_save(&paths, &save);
-		write(1, "looping", 7);
-		sleep(300);
-	}
-
-}
